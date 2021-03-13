ec81f6c79ea4e19af39c715046bd2251a8d66958
kata-gatame <nathan@kata.codes>
2021-03-13 02:14:15 -0500

    Spaces over tabs... ugh.

--- src/commands/game.rs
+++ src/commands/game.rs
@@ -9,20 +9,20 @@ use serenity::framework::standard::{
 
 #[command]
 async fn guess(ctx: &Context, msg: &Message) -> CommandResult {
-	let secret_number = rand::thread_rng().gen_range(1, 101);
+    let secret_number = rand::thread_rng().gen_range(1, 101);
 
     let _ =  msg.reply(ctx, "Please provide your guess...").await;
 
-	if let Some(answer) = &msg.author.await_reply(&ctx).timeout(Duration::from_secs(10)).await {
+    if let Some(answer) = &msg.author.await_reply(&ctx).timeout(Duration::from_secs(10)).await {
         
-		if answer.content.parse::<u32>().unwrap() == secret_number {
+        if answer.content.parse::<u32>().unwrap() == secret_number {
             let _ = answer.reply(ctx, "That's correct!").await;
         } else {
-			if answer.content.parse::<u32>().unwrap() > secret_number {
-            	let _ = answer.reply(ctx, "Wrong, your guess was too big!").await;
-			} else {
-				let _ = answer.reply(ctx, "Wrong, your guess was too small!").await;
-			}
+            if answer.content.parse::<u32>().unwrap() > secret_number {
+                let _ = answer.reply(ctx, "Wrong, your guess was too big!").await;
+            } else {
+                let _ = answer.reply(ctx, "Wrong, your guess was too small!").await;
+            }
         }
     } else {
         let _ =  msg.reply(ctx, "No answer within 10 seconds.").await;
